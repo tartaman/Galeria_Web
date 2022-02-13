4d37434592f138ce8dd52017d651fbbe3e66a164
tartaman <victor.ealpuche@prepafrayangelico.edu.mx>
2022-02-12 18:03:28 -0600

    empezamos

---
+++ Scripts/script.js
@@ -0,0 +1,24 @@
+var i = 0
+function wao(){
+    var imgdoc = document.getElementById('holamundo')
+    var imgs = ['Images/city.jpeg',
+                'Images/Tunel.jpeg']
+    i++
+    if (i >= imgs.length){
+        i = 0
+    }
+    imgdoc.src = imgs[i]
+
+
+}
+function waoRevers(){
+    var imgdoc = document.getElementById('holamundo')
+    var imgs = ['Images/city.jpeg',
+                'Images/Tunel.jpeg']
+    i--
+    console.log(i)
+    if (i < 0){
+        i = imgs.length - 1
+    }
+    imgdoc.src = imgs[i]
+}
