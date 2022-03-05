3e926e18ead9656dad3ec2e5c4053e525343f3a3
tartaman <victor.ealpuche@prepafrayangelico.edu.mx>
2022-03-04 23:00:51 -0600

    oscurito y clarito

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,8 +1,9 @@
 var i = 0
+// Aqui van a poner sus imagenes :D
+var imgs = ['Images/city.jpeg',
+'Images/Tunel.jpeg']
 function wao(){
     var imgdoc = document.getElementById('holamundo')
-    var imgs = ['Images/city.jpeg',
-                'Images/Tunel.jpeg']
     i++
     if (i >= imgs.length){
         i = 0
@@ -11,11 +12,33 @@ function wao(){
 }
 function waoRevers(){
     var imgdoc = document.getElementById('holamundo')
-    var imgs = ['Images/city.jpeg',
-                'Images/Tunel.jpeg']
     i--
     if (i < 0){
         i = imgs.length - 1
     }
     imgdoc.src = imgs[i]
+}
+var clarito = 0
+function claritoOscurito(){
+    if (clarito % 2 == 0){
+        var cambioBoton = document.getElementById('cambio-boton')
+        cambioBoton.innerText= 'Clarito'
+        var oscuros = document.querySelectorAll(['.link-light','.bg-dark'])
+        oscuros.forEach(element => {
+            element.classList.remove('bg-dark','link-light')
+            element.classList.add('bg-light','link-dark')
+        });
+        clarito++
+    } else {
+        var cambioBoton = document.getElementById('cambio-boton')
+        cambioBoton.innerText= 'Oscurito'
+        var oscuros = document.querySelectorAll(['.link-dark','.bg-light'])
+        oscuros.forEach(element => {
+            element.classList.remove('bg-light','link-dark')
+            element.classList.add('bg-dark','link-light')
+        });
+        clarito++
+    }
+}
+function mostrala(){
 }
