513f678c909c515972ef26d7fd7d0a24f2f574e3
tartaman <victor.ealpuche@prepafrayangelico.edu.mx>
2022-03-20 01:44:00 -0600

    indecs

--- Scripts/Victorscript.js
+++ Scripts/Victorscript.js
@@ -2,16 +2,19 @@ var i = 0
 // Aqui van a poner sus imagenes :D
 var imgs = ['Images/ImagenesVictor/Fuecoco.jpg',
 'Images/ImagenesVictor/weisi.jpg',
-'Images/ImagenesVictor/fondo.png']
+'Images/ImagenesVictor/fondo.png',
+'Images/ImagenesVictor/Uhoh.png',
+'Images/ImagenesVictor/Bulletkin\'t.png']
 
 function cambiarDesc(index){
     titleDoc = document.getElementById('title')
     descDoc = document.getElementById('desc')
-    Title = ['Fuecoco', 'Hades']
+    Title = ['Fuecoco', 'Hades','Spiderman:Into The Spiderverse','Terraria','Enter the Gungeon']
     desc = ['Esta imagen es de fuecoco',
      'Aqui una imagen en blaco y negro solo mostrando a zagreo y cerbero',
      'Esta imagen tiene los colores modificados con un ajuste de niveles y curvas',
-     'Todavianohayimagen5']
+     'Ya valiooo...',
+    'Bulletkin\'t']
     titleDoc.innerText =`${Title[index]}`
     descDoc.innerText = `${desc[index]}`
 }
