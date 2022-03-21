6d770427baf2923d9d8e5b38d8b50e1bd0afb1f9
tartaman <victor.ealpuche@prepafrayangelico.edu.mx>
2022-03-21 14:33:59 -0600

    Ultimos Cambios

--- Scripts/Victorscript.js
+++ Scripts/Victorscript.js
@@ -4,17 +4,23 @@ var imgs = ['Images/ImagenesVictor/Fuecoco.jpg',
 'Images/ImagenesVictor/weisi.jpg',
 'Images/ImagenesVictor/fondo.png',
 'Images/ImagenesVictor/Uhoh.png',
-'Images/ImagenesVictor/Bulletkin\'t.png']
+'Images/ImagenesVictor/Bulletkin\'t.png',
+'Images/ImagenesVictor/Yone.png',
+'Images/ImagenesVictor/kirbyred.png',
+'Images/ImagenesVictor/Dst.png']
 
 function cambiarDesc(index){
     titleDoc = document.getElementById('title')
     descDoc = document.getElementById('desc')
-    Title = ['Fuecoco', 'Hades','Spiderman:Into The Spiderverse','Terraria','Enter the Gungeon']
-    desc = ['Esta imagen es de fuecoco',
+    Title = ['Fuecoco', 'Hades','Spiderman:Into The Spiderverse','Terraria','Enter the Gungeon','League of Legends','Kirby','Don\'t starve together']
+    desc = ['Esta imagen es de fuecoco, se le ha alterado los niveles y se le aplico un paso alto para definir mejor a cada uno de los starters',
      'Aqui una imagen en blaco y negro solo mostrando a zagreo y cerbero',
      'Esta imagen tiene los colores modificados con un ajuste de niveles y curvas',
      'Ya valiooo...',
-    'Bulletkin\'t']
+    'Bulletkin\'t',
+    'Este es Yone un campeon del juego League Of Legends al cual se le dejaron las partes con más color para resaltarlas y lo demás se ha dejado en blanco y negro',
+    'Se le cambiaron los ojos a rojos',
+    'Se le aplico un filtro de curvas para aumentarle el brillo a el sol y la antorcha y todo lo demás se ha dejado en blanco y negro']
     titleDoc.innerText =`${Title[index]}`
     descDoc.innerText = `${desc[index]}`
 }
--- Scripts/claritoOscurito.js
+++ Scripts/claritoOscurito.js
@@ -2,7 +2,7 @@ cuenta = 0
 function claritoOscurito(){
     if (cuenta % 2 == 0){
         var cambioBoton = document.getElementById('cambio-boton')
-        cambioBoton.innerText= 'Clarito'
+        cambioBoton.innerText= 'Claro'
         document.getElementById('logo').src ='Images/Logo.png'
         var oscuros = document.querySelectorAll(['.link-light','.bg-dark','.btn-dark'])
         oscuros.forEach(element => {
@@ -12,7 +12,7 @@ function claritoOscurito(){
         cuenta++
     } else {
         var cambioBoton = document.getElementById('cambio-boton')
-        cambioBoton.innerText= 'Oscurito'
+        cambioBoton.innerText= 'Oscuro'
         document.getElementById('logo').src ='Images/LogoBlanco.png'
         var oscuros = document.querySelectorAll(['.link-dark','.bg-light', '.btn-light'])
         oscuros.forEach(element => {
